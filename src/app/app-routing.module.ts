b514cbb2778e4872ddd9f51b58acd7df4bd49c9b
Konstantin Pentarakis <konstantin.pentarakis@adesso.de>
2019-09-27 14:31:40 +0200

    simple book table

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './basic-layout/dashboard/dashboard.component';
+import { BookComponent } from './book/book/book.component';
 
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent }
+  { path: '', component: DashboardComponent },
+  { path: 'book', component: BookComponent }
 ];
 
 @NgModule({
