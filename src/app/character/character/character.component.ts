89dd023ef6b992edbbbc459b632623e0cbfbf558
Konstantin Pentarakis <konstantin.pentarakis@adesso.de>
2019-09-28 15:06:23 +0200

    observable handling

--- src/app/character/character/character.component.ts
+++ src/app/character/character/character.component.ts
@@ -1,23 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Character } from '../model/character';
 import { ActivatedRoute } from '@angular/router';
 import { CharacterService } from '../character.service';
+import { filter, pluck, switchMap, takeUntil } from 'rxjs/operators';
+import { Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'ngb-character',
   templateUrl: './character.component.html',
   styleUrls: ['./character.component.scss']
 })
-export class CharacterComponent implements OnInit {
+export class CharacterComponent implements OnInit, OnDestroy {
 
   character: Character = new Character();
+  isCreateMode = true;
+  destroy: Subject<boolean> = new Subject<boolean>();
 
   constructor(private route: ActivatedRoute, private characterService: CharacterService) {
-    const id = this.route.snapshot.params.id;
-    this.character = this.characterService.read(Number(id));
+
+    this.route.params
+      .pipe(
+        pluck('id'),
+        filter(id => id !== 'create'),
+        switchMap(id => this.characterService.read(Number(id))),
+        takeUntil(this.destroy)
+      )
+      .subscribe(character => this.character = character);
   }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy(): void {
+    this.destroy.next(true);
+    this.destroy.complete();
   }
 
 }
